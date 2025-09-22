51dae5238f7b10d9b80935e38039a651e953dc2d
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2025-09-22 20:35:11 +0100

    expose new type

--- pysimdvec/pysimdvec/__init__.py
+++ pysimdvec/pysimdvec/__init__.py
@@ -1,7 +1,21 @@
 from ._pysimdvec import (
         add,
         sub,
-        mul
+        mul,
+        add_scalar,
+        sub_scalar,
+        scalar_sub,
+        mul_scalar,
+        Array
 )
 
-__all__ = ["add", "sub", "mul"]
+__all__ = [
+        "add", 
+        "sub", 
+        "mul", 
+        "add_scalar", 
+        "sub_scalar", 
+        "scalar_sub", 
+        "mul_scalar", 
+        "Array"
+        ]
