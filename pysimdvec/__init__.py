a9eed0bdb9c3466a49e35ffb112d80a789fdae7f
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2025-09-28 20:19:05 +0100

    expose through python api

--- pysimdvec/__init__.py
+++ pysimdvec/__init__.py
@@ -2,20 +2,26 @@ from ._pysimdvec import (
         add,
         sub,
         mul,
+        div,
         add_scalar,
         sub_scalar,
         scalar_sub,
         mul_scalar,
+        div_scalar,
+        scalar_div,
         Array
 )
 
 __all__ = [
         "add", 
         "sub", 
-        "mul", 
+        "mul",
+        "div",
         "add_scalar", 
         "sub_scalar", 
         "scalar_sub", 
-        "mul_scalar", 
+        "mul_scalar",
+        "div_scalar",
+        "scalar_div",
         "Array"
         ]
